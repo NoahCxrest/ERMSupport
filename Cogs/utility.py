3b683709cdc33a01e562a12c5cd5af0bcae01a72
Noah <117038300+NoahCxrest@users.noreply.github.com>
2023-12-29 17:33:04 -0700

    Update utility.py

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -1,10 +1,23 @@
 import discord
 from discord.ext import commands
+import psutil
+from motor.motor_asyncio import AsyncIOMotorClient
+import json
 
 
 class Utility(commands.Cog):
     def __init__(self, bot_instance):
         self.bot = bot_instance
+        self.config = self.load_config()
+        self.session = self.bot.session
+        self.client = AsyncIOMotorClient(self.config['MONGO_URI'])
+        self.database = self.client["Cronus"]
+        self.collection = self.database["notes"]
+
+    @staticmethod
+    def load_config():
+        with open('./config.json', 'r') as config_file:
+            return json.load(config_file)
 
     @commands.hybrid_command(name="ping", with_app_command=True, description="Get the bots latency")
     async def ping(self, ctx):
@@ -14,6 +27,40 @@ class Utility(commands.Cog):
             color=discord.Color.from_rgb(43, 45, 49)
         )
         await ctx.reply(embed=embed)
+
+    @commands.hybrid_command(name="help", with_app_command=True, description="Get a list of commands")
+    async def get_commands(self, ctx):
+        try:
+            commands_by_cog = self.bot.commands_cache
+
+            embed = discord.Embed(title="Command List", color=discord.Color.from_rgb(43, 45, 49))
+            for cog_name, cog_commands in sorted(commands_by_cog.items()):
+                embed.add_field(name=cog_name, value="\n".join(cog_commands), inline=False)
+
+            if not embed.fields:
+                embed.description = "Cronus is still loading commands; please try again in a few seconds."
+
+            await ctx.send(embed=embed)
+
+        except Exception as e:
+            await ctx.send(f"An error occurred while fetching the command list: {e}")
+
+    @commands.hybrid_command(name="about", with_app_command=True, description="Learn about Cronus")
+    async def about(self, ctx):
+        embed = discord.Embed(
+            title="About Cronus",
+            description="Cronus is a Discord bot created by [Noah](https://discord.com/users/459374864067723275) "
+                        "to help with the management of the ERM Systems Discord server.",
+            color=discord.Color.from_rgb(43, 45, 49)
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar)
+        process = psutil.Process()
+        memory_usage = process.memory_info().rss / 1024 ** 2
+        embed.add_field(name="RAM Usage", value=f"{memory_usage:.2f} MB", inline=True)
+        embed.add_field(name="CPU Usage", value=f"{psutil.cpu_percent()}%", inline=True)
+        embed.add_field(name="Loaded Cogs", value=len(self.bot.cogs), inline=True)
+
+        await ctx.send(embed=embed)
 
 
 async def setup(bot_instance):
